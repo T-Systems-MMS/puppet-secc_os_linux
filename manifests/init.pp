38c4aac9728ee07dfbd1fe9dfa9d89fe935f54d4
Robert Knochenhauer <R.Knochenhauer@t-systems.com>
2016-04-01 07:44:56 +0200

    semantik

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,8 +11,8 @@ class secc_os_linux (
   $ext_test_kitchen_run           = false,
   $ext_rootsh_enabled             = true,
   $ext_bash_ps1                   = 'PS1="\[$(tput bold)\]\[$(tput setaf 3)\]\\u\[$(tput setaf 4)\]@\[$(tput setaf 2)\]\\h\[$(tput setaf 4)\]:\[$(tput setaf 7)\]\\w\[$(tput setaf 4)\]\\$ \[$(tput sgr0)\]"',
-  $ext_stop_and_disable_services      = [ 'acpid', 'anacron', 'cups', 'dhcpd', 'network-remotefs', 'haldaemon', 'lm_sensors', 'mdmonitor', 'netconsole', 'netfs', 'nfs', 'ntpdate', 'oddjobd', 'portmap', 'portreserve', 'qpidd', 'quota_nld', 'rdisc', 'rhnsd', 'rhsmcertd', 'saslauthd', 'sendmail', 'smartd', 'sysstat', 'vsftpd' ],
-  $ext_stop_services                  = [ 'nfslock', 'rpcgssd', 'rpcidmapd', 'rpcsvcgssd' ],
+  $ext_stop_and_disable_services  = [ 'acpid', 'anacron', 'cups', 'dhcpd', 'network-remotefs', 'haldaemon', 'lm_sensors', 'mdmonitor', 'netconsole', 'netfs', 'nfs', 'ntpdate', 'oddjobd', 'portmap', 'portreserve', 'qpidd', 'quota_nld', 'rdisc', 'rhnsd', 'rhsmcertd', 'saslauthd', 'sendmail', 'smartd', 'sysstat', 'vsftpd' ],
+  $ext_stop_services              = [ 'nfslock', 'rpcgssd', 'rpcidmapd', 'rpcsvcgssd' ],
 ){
 
   $tftp_server_package_status = hiera(tftp_server_package_status, $ext_tftp_server_package_status)
