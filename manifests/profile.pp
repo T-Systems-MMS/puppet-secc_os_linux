0468bb72eebfd4f1e2b82a37407c7205a654b13f
Sebastian Gumprich <Sebastian.Gumprich@t-systems.com>
2016-09-22 10:47:39 +0200

    fix umask setting

--- manifests/profile.pp
+++ manifests/profile.pp
@@ -2,25 +2,18 @@
 # copied from puppet os hardening module - see hardening.io for details
 class secc_os_linux::profile {
 
-  # this is for interactive logins
-  file_line { 'etc_profile_umask' :
+  file { '/etc/profile.d/umask.sh':
     ensure => present,
-    path   => '/etc/profile',
-    line   => 'umask 027',
-    match  => 'umask 02.*',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/secc_os_linux/etc/profile.d/umask.sh',
   }
 
   # non interactive sessions could effect application behaviours and are not affected by default
   # this is for non-interactive logins
 
   if ( $::operatingsystem == 'RedHat' ) or ( $::operatingsystem == 'CentOS' )  {
-          file_line { 'etc_rhel_bashrc_umask' :
-            ensure => present,
-            path   => '/etc/bashrc',
-            line   => 'umask 027',
-            match  => 'umask 02.*',
-          }
-
           file_line { 'etc_profile_rootsh' :
             ensure => present,
             path   => '/etc/profile',
