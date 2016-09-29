a5150617eb7f6b2794c4b9723b73faf20d345a97
joko <joerg.kohlsdorf@t-systems.com>
2016-09-29 15:20:58 +0200

    remove commented out code that was moved to init.pp and add some new comments for better understanding

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,6 +1,3 @@
-# config for service availability and status
-# copied from puppet os hardening module - see hardening.io for details
-
 class secc_os_linux::packages (
   $tftp_server_package_status,
   $xinetd_package_status,
@@ -8,8 +5,6 @@ class secc_os_linux::packages (
 ){
 
   # SoC - Requirement 3.01-3 - Nicht benötigte Software darf nicht installiert oder muss deinstalliert werden.
-  #    deinstall: telnet, abrt
-
 
   # own handling of alsa-firmware, because dependency between those package have to be removed "yum -y erase" and it reduces the risk of unwanted deinstallations
   # not working ...
@@ -28,68 +23,12 @@ class secc_os_linux::packages (
       }
   }
   
-  ensure_packages( ['tftp-server'], {'ensure' => $tftp_server_package_status} )
-  ensure_packages( ['xinetd'], {'ensure' => $xinetd_package_status} )
-
-  ensure_packages(
-    [
-      # 'abrtd',
-      # 'autofs',
-      # 'avahi-daemon',
-      # 'cpuspeed',  #frequency scaling not supported under xen kernels / vmware unknown / for physical servers it could be relevant
-      # 'ftp',
-      # 'inetd',
-      # 'kdump',
-      # 'rlogin',
-      # 'rsh-server',
-      # 'telnet-server',
-      # 'ypserv',
-      # 'ypbind',
-      # remove default installed firmware
-      # 'aic94xx-firmware',
-      # 'atmel-firmware',
-      # 'adaptec-firmware',
-      # 'bfa-firmware',
-      # 'brocade-firmware',
-      # 'icom-firmware',
-      # 'ipw-firmware',
-      # 'ipw2100-firmware',
-      # 'ipw2200-firmware',
-      # 'ivtv-firmware',
-      # 'iwl100-firmware',
-      # 'iwl105-firmware',
-      # 'iwl135-firmware',
-      # 'iwl1000-firmware',
-      # 'iwl2000-firmware',
-      # 'iwl2030-firmware',
-      # 'iwl3160-firmware',
-      # 'iwl3945-firmware',
-      # 'iwl4965-firmware',
-      # 'iwl5000-firmware',
-      # 'iwl5150-firmware',
-      # 'iwl6000-firmware',
-      # 'iwl6000g2a-firmware',
-      # 'iwl6000g2b-firmware',
-      # 'iwl6050-firmware',
-      # 'iwl7260-firmware',
-      # 'iwl7265-firmware',
-      # 'libertas-sd8686-firmware',
-      # 'libertas-sd8787-firmware',
-      # 'libertas-usb8388-firmware',
-      # 'mpt-firmware',
-      # 'ql2100-firmware',
-      # 'ql2200-firmware',
-      # 'ql23xx-firmware',
-      # 'ql2400-firmware',
-      # 'ql2500-firmware',
-      # 'rt61pci-firmware',
-      # 'rt73usb-firmware',
-      # 'xorg-x11-drv-ati-firmware',
-      # 'zd1211-firmware',
-      $remove_packages,
-    ],
-      {'ensure' => 'absent'}
-  )
+  # uninstall unwanted packages, configured as default parameter to $ext_remove_packages in init.pp
+  ensure_packages( $remove_packages, {'ensure' => 'absent'} )
+  
+  # uninstalled by default parameter 'absent' in init.pp
+  ensure_packages( 'tftp-server', {'ensure' => $tftp_server_package_status} )
+  ensure_packages( 'xinetd', {'ensure' => $xinetd_package_status} )
 
   # packages we need for every service
   ensure_packages( ['curl', 'wget'], {'ensure' => 'present'} )
