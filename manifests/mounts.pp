2163ecd373790dcc9a77d82af60b9234f8d20879
Sebastian Gumprich <Sebastian.Gumprich@t-systems.com>
2017-05-03 13:19:59 +0200

    change mount-option

--- manifests/mounts.pp
+++ manifests/mounts.pp
@@ -1,54 +1,31 @@
 # hardening mountpoints
-class secc_os_linux::mounts (
-  $secure_mountpoint_tmp,
-  $secure_mountpoint_var,
-  $secure_mountpoint_var_tmp,
-  $secure_mountpoint_home,
-  $test_kitchen_run,
-){
+class secc_os_linux::mounts (){
 
-  # noexec on /tmp prevents test-kitchen :/
-  if ( $test_kitchen_run ) {
-    $tmp_mount_options = 'defaults,nodev,nosuid'
-  } else {
-    $tmp_mount_options = 'defaults,noexec,nodev,nosuid'
-  }
-
-
-  if ( $secure_mountpoint_tmp ) {
+  if $secure_mountpoint_tmp {
     mount { '/tmp':
       ensure  => 'mounted',
-      options => $tmp_mount_options,
-      target  => '/etc/fstab',
-      pass    => '2',
+      options => $mount_options_tmp,
     }
   }
 
-  if ( $secure_mountpoint_var ) {
+  if $secure_mountpoint_var_tmp {
+    mount { '/var/tmp':
+      ensure  => 'mounted',
+      options => $mount_options_var_tmp,
+    }
+  }
+
+  if $secure_mountpoint_var {
     mount { '/var':
       ensure  => 'mounted',
-      options => 'defaults,noexec,nodev,nosuid',
-      target  => '/etc/fstab',
-      pass    => '2',
+      options => $mount_options_var,
     }
   }
 
-  if ( $secure_mountpoint_home ) {
+  if $secure_mountpoint_home {
     mount { '/home':
       ensure  => 'mounted',
-      options => 'defaults,nodev',
-      target  => '/etc/fstab',
-      pass    => '2',
-    }
-  }
-
-  if ( $secure_mountpoint_var_tmp ) {
-    mount {'/var/tmp':
-      ensure  => 'mounted',
-      device  => '/tmp',
-      fstype  => 'none',
-      options => 'bind',
-      pass    => '0',
+      options => $mount_options_home,
     }
   }
 
