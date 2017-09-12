ffcbf4887fb1c83acf045a1b9ced8d6e05572bb0
Jens Grossmann <Jens.Grossmann@t-systems.com>
2017-09-12 10:44:42 +0200

    ASC-236 Extend inputrc to support normal key up and down for history search

--- manifests/inputrc.pp
+++ manifests/inputrc.pp
@@ -2,18 +2,34 @@
 
 class secc_os_linux::inputrc {
 
-  file_line { 'history_search_backward' :
+  file_line { 'history_search_backward_for_page_up' :
     ensure => present,
     path   => '/etc/inputrc',
     line   => '"\e[5~": history-search-backward',
     match  => '^"\\\e\[5~":',
   }
 
-  file_line { 'history_search_forward' :
+  file_line { 'history_search_forward_for_page_down' :
     ensure => present,
     path   => '/etc/inputrc',
     line   => '"\e[6~": history-search-forward',
     match  => '^"\\\e\[6~":',
+  }
+
+  file_line { 'history_search_backward_for_up' :
+    ensure => present,
+    path   => '/etc/inputrc',
+    line   => '"\e[A": history-search-backward',
+    match  => '^"\\\e\[A":',
+    after  => '^"\\\e\[6~":',
+  }
+
+  file_line { 'history_search_forward_for_down' :
+    ensure => present,
+    path   => '/etc/inputrc',
+    line   => '"\e[B": history-search-forward',
+    match  => '^"\\\e\[B":',
+    after  => '^"\\\e\[6~":',
   }
 
 }
