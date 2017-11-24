f304cef9c529dbd2744791d1e2420341622ea141
crgr <Christopher.Grau@t-systems.com>
2017-11-24 10:39:27 +0100

    parameterize umask.sh

--- manifests/profile.pp
+++ manifests/profile.pp
@@ -1,13 +1,15 @@
 # config for umask setting
 # copied from puppet os hardening module - see hardening.io for details
-class secc_os_linux::profile {
+class secc_os_linux::profile(
+  $profile_umask,
+  ) {
 
-  file { '/etc/profile.d/umask.sh':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/secc_os_linux/etc/profile.d/umask.sh',
+  file {'/etc/profile.d/umask.sh':
+    ensure  => 'present',
+    content => template('secc_os_linux/etc/profile.d/umask.sh.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
   }
 
   # non interactive sessions could effect application behaviours and are not affected by default
